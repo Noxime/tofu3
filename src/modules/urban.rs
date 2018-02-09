daab86e89ad3f15cceaf74c6150d1a7365208eaa
noxim <aaro.peramaa@gmail.com>
2018-02-09 17:35:03 +0200

    [urban] Fixed issues where text was > 2000 characters or there wasn't an example available

--- src/modules/urban.rs
+++ src/modules/urban.rs
@@ -6,48 +6,58 @@ use serenity::utils::Colour;
 command!(urban(_ctx, msg, args) {
     let _ = msg.channel_id.broadcast_typing();
     // Search term
-    if let Ok(query) = args.single::<String>() {
-
-        // use Zeyla's lib for accessing UB nicely
-        // TODO: See if this will be a performance issue (probably not tho)
-        let client = Client::new();
-        let response = match client.define(&query) {
-            Ok(r) => r,
-            Err(why) => {
-                error!("Failed to access UB: {:#?}", why);
-                None // TODO: Tell user about error rather than returning none
-            }
-        };
-
-        // did we even find anything
-        match response {
-            Some(def) => {
-                let _ = msg.channel_id.send_message(|f| f.embed(|m| m
-                    .color(Colour::fooyoo())
-                    .title(&format!("Definition of {}", &def.word))
-                    .url(&def.permalink)
-                    .description(&def.definition)
-                    .field("Example", &def.example, true)
-                    .field("Votes", format!("👍: **{}** 👎: **{}**", 
-                        &def.thumbs_up, &def.thumbs_down), true)
-                    .footer(|f| f
-                        .text(&format!("Defined by {}", def.author)))
-                    ));
-            },
-            None => {
-                let _ = msg.channel_id.send_message(|f| f.embed(|m| m
-                    .color(Colour::gold())
-                    .title(format!("Could not find \"{}\"", query))
-                    .description(format!(
-                        "Could not find \"{}\" on Urban Dictionary. Are you \
-                        sure you wrote it correctly?",
-                        query))));
-            }
+    let query = args.full();
+    // use Zeyla's lib for accessing UB nicely
+    // TODO: See if this will be a performance issue (probably not tho)
+    let client = Client::new();
+    let response = match client.define(&query) {
+        Ok(r) => r,
+        Err(why) => {
+            error!("Failed to access UB: {:#?}", why);
+            None // TODO: Tell user about error rather than returning none
         }
-    } else {
-        let _ = msg.channel_id.send_message(|f| f.embed(|m| m
-            .color(Colour::red())
-            .title("Incorrect usage")
-            .description("Please provide a search term!")));
+    };
+
+    // did we even find anything
+    match response {
+        Some(def) => {
+            // discord only accepts 2000 characters. 1800 should give us enough headroom
+            // for our example field to fit
+            let mut s = def.definition.clone();
+            if s.len() > 1800 {
+                s.truncate(1800);
+            }
+
+            // discord doesn't allow empty fields, so add placeholder incase no example
+            // TODO: I guess we just shouldnt send a field incase this is empty
+            let mut e = def.example.clone();
+            if e.is_empty() {
+                e = "<none>".into();
+            }
+            
+            match msg.channel_id.send_message(|f| f.embed(|m| m
+                .color(Colour::fooyoo())
+                .title(&format!("Definition of {}", &def.word))
+                .url(&def.permalink)
+                .description(s)
+                .field("Example", e, true)
+                .field("Votes", format!("👍: **{}** 👎: **{}**", 
+                    &def.thumbs_up, &def.thumbs_down), true)
+                .footer(|f| f
+                    .text(&format!("Defined by {}", def.author)))
+                )) {
+                Ok(_) => {},
+                Err(why) => error!("Sending UB failed: {:#?}", why)
+            }
+        },
+        None => {
+            let _ = msg.channel_id.send_message(|f| f.embed(|m| m
+                .color(Colour::gold())
+                .title(format!("Could not find \"{}\"", query))
+                .description(format!(
+                    "Could not find \"{}\" on Urban Dictionary. Are you \
+                    sure you wrote it correctly?",
+                    query))));
+        }
     }
 });
