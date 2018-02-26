f4a8bada5b8e00a725f02c55354fe59d4ce0da7a
noxim <aaro.peramaa@gmail.com>
2018-02-26 18:24:33 +0200

    [monitoring] Removed unwraps and cleaned up code

--- src/dog.rs
+++ src/dog.rs
@@ -4,35 +4,57 @@ lazy_static! {
     pub static ref DOG: DogClient = DogClient::new(Options::default()).unwrap();
 }
 
-fn vec() -> Vec<String> {
-    vec!["tofubot".to_string(), format!("profile:{}", env!("CARGO_PKG_PROFILE"))]
+fn vec(tags: Vec<String>) -> Vec<String> {
+    let v = vec![
+        "tofubot".to_string(), 
+        format!("profile:{}", env!("CARGO_PKG_PROFILE"))
+    ];
+    [&v[..], &tags[..]].concat()
 }
 
 #[allow(dead_code)]
 pub fn event(title: &str, content: &str, tags: Vec<String>) {
-    DOG.event(title, content, [&vec()[..], &tags[..]].concat()).unwrap();
+    if let Err(why) = DOG.event(title, content, vec(tags)) {
+        error!("DataDog event failed: {}", why);
+        error!("Title and content: {}, {}", title, content);
+    }
 }
 
 #[allow(dead_code)]
 pub fn incr(name: &str, tags: Vec<String>) {
-    DOG.incr(name, [&vec()[..], &tags[..]].concat()).unwrap();
+    if let Err(why) = DOG.incr(name, vec(tags)) {
+        error!("DataDog increment failed: {}", why);
+        error!("Name: {}", name);
+    }
 }
 #[allow(dead_code)]
 pub fn decr(name: &str, tags: Vec<String>) {
-    DOG.decr(name, [&vec()[..], &tags[..]].concat()).unwrap();
+    if let Err(why) = DOG.decr(name, vec(tags)) {
+        error!("DataDog decrement failed: {}", why);
+        error!("Name: {}", name);
+    }
 }
 
 #[allow(dead_code)]
 pub fn set(name: &str, val: i64, tags: Vec<String>) {
-    DOG.gauge(name, &val.to_string(), [&vec()[..], &tags[..]].concat()).unwrap();
+    if let Err(why) = DOG.gauge(name, &val.to_string(), vec(tags)) {
+        error!("DataDog set failed: {}", why);
+        error!("Name and value: {}, {}", name, val);
+    }
 }
 
 #[allow(dead_code)]
 pub fn time<F: FnOnce()>(name: &str, tags: Vec<String>, block: F) {
-    DOG.time(name, tags, block).unwrap();
+    if let Err(why) = DOG.time(name, vec(tags), block) {
+        error!("DataDog time failed: {}", why);
+        error!("Title: {}", name);
+    }
 }
 
 #[allow(dead_code)]
 pub fn timing(name: &str, val: i64, tags: Vec<String>) {
-    DOG.timing(name, val, [&vec()[..], &tags[..]].concat()).unwrap();
+    if let Err(why) = DOG.timing(name, val, vec(tags)) {
+        error!("DataDog timing failed: {}", why);
+        error!("Title and value: {}, {}", name, val);
+    }
 }
