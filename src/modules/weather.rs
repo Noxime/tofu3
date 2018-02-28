b4e61b8826c23e1179c27f74e089ecc8dd854fda
noxim <aaro.peramaa@gmail.com>
2018-02-28 11:10:24 +0200

    [weather] wip weather access

--- src/modules/weather.rs
+++ src/modules/weather.rs
@@ -1,5 +1,5 @@
 use reqwest::Client;
-use forecast::{ApiClient, ForecastRequestBuilder};
+use forecast::{ApiClient, ForecastRequestBuilder, ApiResponse};
 use serenity::utils::Colour;
 
 use utils::deser_string;
@@ -31,9 +31,9 @@ command!(weather(_ctx, msg, args) {
     let data: Vec<Nomi> = unres_cmd!(
         unres_cmd!(req.get(&format!("
             https://nominatim.openstreetmap.org/search/kamppi?format=jsonv2&q={}
-            ", args.full())
-            ).send(), "nominatim request failed")
-        .json(), "nominatim deserialize failed");
+            ", args.full())).send(), 
+        "nominatim request failed").json(), 
+    "nominatim deserialize failed");
 
     // unparse that shit
     let nomi = unopt_cmd!(data.get(0), "nominatim had 0 results");
@@ -42,12 +42,23 @@ command!(weather(_ctx, msg, args) {
     let country = name.last().unwrap_or("unknown").trim();
 
     let api = ApiClient::new(&req);
-    let cast = ForecastRequestBuilder::new(&key, nomi.lat, nomi.lon).build();
+    let cast: ApiResponse = unres_cmd!(
+        unres_cmd!(
+            api.get_forecast(
+            ForecastRequestBuilder::new(&key, nomi.lat, nomi.lon).build()),
+        "forecast request failed").json(),
+    "forecast deserialize failed");
+    let cur = unopt_cmd!(cast.currently, "no weather data");
 
     unres_cmd!(msg.channel_id.send_message(|m| m.embed(|e| e
         .color(Colour::fooyoo())
         .title("Weather search results")
         .description(format!("Showing results for **{}**, **{}**", 
             place, country))
+        .field("Temperature", format!("\
+            Current: **{}**\nHigh/low: **{}**/**{}**",
+            cur.temperature.unwrap_or(0f64),
+            cur.temperature_high.unwrap_or(0f64), 
+            cur.temperature_low.unwrap_or(0f64)), true)
     )), "msg failed");
 });
