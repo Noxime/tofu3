2ad7c1aa05d821f6916fe4d430047bd3ba389a7b
noxim <aaro.peramaa@gmail.com>
2018-02-28 21:16:35 +0200

    [weather] basic functionality working

--- src/modules/weather.rs
+++ src/modules/weather.rs
@@ -1,10 +1,11 @@
 use reqwest::Client;
-use forecast::{ApiClient, ForecastRequestBuilder, ApiResponse};
+use forecast::{ApiClient, ForecastRequestBuilder, ApiResponse, Lang, Units};
 use serenity::utils::Colour;
 
 use utils::deser_string;
 
 use std::env;
+use std::f64;
 
 // fetch the weather information for a location
 command!(weather(_ctx, msg, args) {
@@ -13,11 +14,7 @@ command!(weather(_ctx, msg, args) {
     // struct for Nominatim data
     #[derive(Debug, Serialize, Deserialize)]
     struct Nomi {
-        #[serde(with = "deser_string")]
-        place_id: usize,
         licence: String,
-        #[serde(with = "deser_string")]
-        osm_id: usize,
         #[serde(with = "deser_string")]
         lat: f64,
         #[serde(with = "deser_string")]
@@ -38,27 +35,75 @@ command!(weather(_ctx, msg, args) {
     // unparse that shit
     let nomi = unopt_cmd!(data.get(0), "nominatim had 0 results");
     let mut name = nomi.display_name.split(", ");
-    let place = name.next().unwrap_or("unknown").trim();
-    let country = name.last().unwrap_or("unknown").trim();
+    let place = name.next().unwrap_or("Somewhere").trim();
+    let country = name.last().unwrap_or("Earth").trim();
 
     let api = ApiClient::new(&req);
     let cast: ApiResponse = unres_cmd!(
         unres_cmd!(
             api.get_forecast(
-            ForecastRequestBuilder::new(&key, nomi.lat, nomi.lon).build()),
+            ForecastRequestBuilder::new(&key, nomi.lat, nomi.lon)
+                .lang(Lang::English)
+                .units(Units::SI)
+                .build()),
         "forecast request failed").json(),
     "forecast deserialize failed");
+
+
     let cur = unopt_cmd!(cast.currently, "no weather data");
+    debug!("{:#?}", cur);
+    
+    // unpack our values
+    let temp = cur.temperature.unwrap_or(f64::NAN);
+    let high = cur.temperature_high.unwrap_or(f64::NAN);
+    let low = cur.temperature_low.unwrap_or(f64::NAN);
+    let wind = cur.wind_speed.unwrap_or(f64::NAN);
+    let feels = cur.apparent_temperature.unwrap_or(f64::NAN);
+    let vis = cur.visibility.unwrap_or(20f64);
+
+    // generate a nice text
+    // example:
+    // It is currently -15.8°C, with wind of 3.5 m/s bringing that up to a 
+    // freezing -23.1°C. The sky looks clear with a visibility of about 10 km.
+    let summary = format!("\
+        _It is currently **{:.1}°C**, with wind of **{:.1} m/s** \
+        bringing that {} to {}**{:.1}°C**. {} about **{:.0} km**._ ",
+        temp,
+        wind,
+        if feels > temp { "up" } else { "down" },
+        match feels as isize {
+            -100 ... -40 => "a frightening ",
+            -39 ... -20 => "a freezing ",
+            -19 ... -5 => "a chilly ",
+            -4 ... 11 => "a mild ",
+            18 ... 24 => "a warm ",
+            25 ... 32 => "a toasty ",
+            33 ... 37 => "a burning ",
+            38 ... 60 => "a melting hot ",
+            _ => "",
+        },
+        feels,
+        cur.summary.map(|v| 
+            format!("The sky looks {} with a visibility of", v.to_lowercase()))
+            .unwrap_or("The visibility is".into()),
+        vis,
+    );
 
     unres_cmd!(msg.channel_id.send_message(|m| m.embed(|e| e
         .color(Colour::fooyoo())
-        .title("Weather search results")
-        .description(format!("Showing results for **{}**, **{}**", 
+        .title(format!("Weather in **{}**, **{}**", 
             place, country))
+        .description(summary)
         .field("Temperature", format!("\
-            Current: **{}**\nHigh/low: **{}**/**{}**",
-            cur.temperature.unwrap_or(0f64),
-            cur.temperature_high.unwrap_or(0f64), 
-            cur.temperature_low.unwrap_or(0f64)), true)
+            Current: **{:.2}°C**\nHigh/low: **{:.2}°C**/**{:.2}°C**",
+            temp,
+            high, 
+            low
+            ), true)
+        .field("Wind chill", format!("\
+            Feels like: **{:.2}°C**\nWind speed: **{:.2}m/s**",
+            feels,
+            wind
+            ), true)
     )), "msg failed");
 });
